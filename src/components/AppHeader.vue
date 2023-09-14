0c01fff6752a81f93455671328973c0191a9341b
umbertofaenza <umberto.faenza@gmail.com>
2023-09-14 15:30:30 +0200

    feat: add basic search bar to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,22 @@
 <script></script>
 
 <template>
-  <div class="container">
-    <h2>APP HEADER</h2>
+  <div class="container mt-3">
+    <!-- search bar -->
+    <form class="row g-3">
+      <div class="col-auto">
+        <!-- input -->
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter a title..."
+        />
+      </div>
+      <div class="col-auto">
+        <!-- button -->
+        <button type="submit" class="btn btn-primary mb-3">Search</button>
+      </div>
+    </form>
   </div>
 </template>
 
