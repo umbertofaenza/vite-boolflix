c20a2f73e57c650725b78bc2463f8ba020597385
umbertofaenza <umberto.faenza@gmail.com>
2023-09-14 16:17:03 +0200

    feat: fetch movies by searchedterm (console print)

--- src/App.vue
+++ src/App.vue
@@ -13,29 +13,30 @@ export default {
   data() {
     return {
       store,
+      moviesQueryURL: "",
     };
   },
 
   components: { AppHeader, AppMain },
 
   methods: {
-    fetchMoviesByQuery(endpoint) {
-      axios.get(endpoint).then((response) => {
+    fetchMoviesByQuery(searchedTerm) {
+      this.moviesQueryURL = `https://api.themoviedb.org/3/search/movie?api_key=5c764612f10030f97b723e24778692e4&query=${searchedTerm}`;
+
+      axios.get(this.moviesQueryURL).then((response) => {
         store.moviesData = response.data.results;
+
+        console.log(response.data.results);
       });
     },
   },
 
-  created() {
-    this.fetchMoviesByQuery(
-      "https://api.themoviedb.org/3/search/movie?api_key=5c764612f10030f97b723e24778692e4&query=anelli"
-    );
-  },
+  created() {},
 };
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader @search-submit="fetchMoviesByQuery" />
   <AppMain />
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,29 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      searchedTerm: "",
+    };
+  },
+
+  emits: ["search-submit"],
+};
+</script>
 
 <template>
   <div class="container mt-3">
     <!-- search bar -->
-    <form class="row g-3">
+    <form
+      class="row g-3"
+      @submit.prevent="$emit('search-submit', searchedTerm)"
+    >
       <div class="col-auto">
         <!-- input -->
         <input
           type="text"
           class="form-control"
           placeholder="Enter a title..."
+          v-model="searchedTerm"
         />
       </div>
       <div class="col-auto">
