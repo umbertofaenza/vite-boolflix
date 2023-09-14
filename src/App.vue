6f9b843cdc6cf5d4256fc5d30508839f89ccb357
umbertofaenza <umberto.faenza@gmail.com>
2023-09-14 15:48:40 +0200

    feat: add store and method to fetch test data

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,35 @@
 // axios
 import axios from "axios";
 
+// store
+import { store } from "./data/store";
+
 // components
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
   components: { AppHeader, AppMain },
+
+  methods: {
+    fetchMoviesByQuery(endpoint) {
+      axios.get(endpoint).then((response) => {
+        store.moviesData = response.data.results;
+      });
+    },
+  },
+
+  created() {
+    this.fetchMoviesByQuery(
+      "https://api.themoviedb.org/3/search/movie?api_key=5c764612f10030f97b723e24778692e4&query=anelli"
+    );
+  },
 };
 </script>
 
