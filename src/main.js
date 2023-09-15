94db914d326aa198d7ffb705b74e87308146b37a
umbertofaenza <umberto.faenza@gmail.com>
2023-09-15 16:39:21 +0200

    install and import fontawesome

--- src/main.js
+++ src/main.js
@@ -4,4 +4,12 @@ import App from "./App.vue";
 // bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).mount("#app");
+// fontawesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+// import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+// add icons to the library
+library.add();
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
